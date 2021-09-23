1e1b7774c3265f278c018428835ee2cee1019cf0
Unknownbotcreator <90542170+Rohithbotusers@users.noreply.github.com>
2021-09-23 09:39:22 +0530

    Update __main__.py

--- innexiaBot/__main__.py
+++ innexiaBot/__main__.py
@@ -808,7 +808,7 @@ def main():
 
     if SUPPORT_CHAT is not None and isinstance(SUPPORT_CHAT, str):
         try:
-            dispatcher.bot.sendMessage(f"@{SUPPORT_CHAT}", "My fafnir is ready for battle 🔥")
+            dispatcher.bot.sendMessage(f"@{SUPPORT_CHAT}", " My fafnir is ready for battle 🔥")
         except Unauthorized:
             LOGGER.warning(
                 "Bot isnt able to send message to support_chat, go and check!"
